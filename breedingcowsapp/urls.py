b013b0880f6427f83b8ec6fbe74aae19db9d3f79
ABOrdonez <agustina.ordonez@mercadolibre.com>
2019-10-14 20:33:42 -0300

    Added view and template for detailed blog post as well as CSS for the site.

--- breedingcowsapp/urls.py
+++ breedingcowsapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.breeding_cows_list, name='breeding_cows_list'),
+    path('breeding_cow/<int:pk>/', views.breeding_cow_detail, name='breeding_cow_detail'),
+
 ]
--- breedingcowsapp/views.py
+++ breedingcowsapp/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-from django.shortcuts import render
 from .models import BreedingCows
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 
 def breeding_cows_list(request):
     breeding_cows = BreedingCows.objects.filter(entry_date__lte=timezone.now()).order_by('entry_date')
-    return render(request, 'breedingcowsapp/breeding_cows_list.html', {'breeding_cows':breeding_cows})
+    return render(request, 'breedingcowsapp/breeding_cows_list.html', {'breeding_cows': breeding_cows})
+
+
+def breeding_cow_detail(request, pk):
+    breeding_cow = get_object_or_404(BreedingCows, pk=pk)
+    return render(request, 'breedingcowsapp/breeding_cow_detail.html', {'breeding_cow': breeding_cow})
